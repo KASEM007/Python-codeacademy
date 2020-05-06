f7e9dd07ad872789824f136d5cd93ac6925f6176
Kasem007 <kasem4coding@gmail.com>
2020-05-06 19:50:59 -0400

    new Cahllenge

--- Projects/user&password_generator.py
+++ Projects/user&password_generator.py
@@ -41,7 +41,7 @@ def username_generator(first_name, last_name):
 def password_generator(user_name):
     password = ""
     for i in range(0, len(user_name)):
-        print(i, i - 1, user_name[i - 1])
+        # print(i, i - 1, user_name[i - 1])
         password += user_name[i - 1]
     return password
 
