bac1d028456ca6a191193e62864eb57f8d56d892
Kasem007 <kasem4coding@gmail.com>
2020-05-27 11:20:18 -0400

    jupyterlab project

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
-alpha = "abcdefghijklmnobqrstuvwxyz"
-punctuation = "., ?, !"
-message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!"
-translate_message = ""
+# def nearest_value(values, one):
+#     for i in values:
+#         if one in values:
+#             return one
+#         elif (one - 1 in values) and (one + 1 in values):
+#             return one - 1
+#         else:
+#             if one + 1 in values:
+#                 return one + 1
 
-# for char in message:
-#     if char not in punctuation:
-#         char_idx = alpha.find(char)
-#         translate_message += alpha[(char_idx + 10) % 26]
-#     else:
-#         translate_message += char
 
-# print(translate_message)
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 9))  # == 10
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 8))  # == 7
+# print(nearest_value({4, 8, 10, 11, 12, 17}, 9))  # == 8
+# print(nearest_value({4, 9, 10, 11, 12, 17}, 9))  # == 9
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 0))  # == 4
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 100))  # == 17
+# print(nearest_value({5, 10, 8, 12, 89, 100}, 7))  # == 8
+# print(nearest_value({-1, 2, 3}, 0))  # == -1
 
-for char in message:
-    if char in punctuation:
-        translate_message += char
-    else:
-        char_idx = alpha.find(char)
-        translate_message += alpha[(char_idx + 10) % 26]
-print(translate_message)
+x = 10
+print(abs(10))
