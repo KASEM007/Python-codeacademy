3544c4e1ed3203e3d5e2a7a130a26d21cafc7d91
Kasem007 <kasem4coding@gmail.com>
2020-07-08 22:05:43 -0400

    basta Resturant

---
+++ classes/users_class.py
@@ -0,0 +1,28 @@
+class UserGroup:
+  def __init__(self, users, permissions):
+    self.user_list = users
+    self.permissions = permissions
+
+  def __iter__(self):
+    return iter(self.user_list)
+
+  def __len__(self):
+    return len(self.user_list)
+
+  def __contains__(self, user):
+    return user in self.user_list
+
+
+class User:
+    def __init__(self, user_name):
+        self.user_name = user_name
+
+Dianna = User('Dianna')
+Frank = User('Frank')
+Jenn = User('Jenn')
+
+can_edit = UserGroup([Dianna, Frank], {'can edit pag':True})
+can_delet = UserGroup([Dianna, Jenn], {'can delete posts': True})
+
+print(len(can_edit))
+print(UserGroup.contains(self.can_edit))
